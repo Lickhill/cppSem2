ad507d3f98e0ecbbb2016e36208ae363fd818efd
lickhill <likhilnm17103@gmail.com>
2024-01-12 14:53:20 +0530

    cpp day1

---
+++ subjectavg.cpp
@@ -0,0 +1,17 @@
+// WAP to find average marks of five subjects of a student in a class.
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int subjects[5];
+    int sum = 0;
+    for (int i = 0; i < 5; i++)
+    {
+        cout << "Enter the marks of subject" << i + 1 << ": ";
+        cin >> subjects[i];
+        sum += subjects[i];
+    }
+
+    cout << "The average of all the subjects is: " << sum;
+}
